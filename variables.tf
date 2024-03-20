9482169b9c9b28a799ca381e5edfd36adb1dac8c
Samuelguerrero1184 <samuel040902@hotmail.com>
2024-03-20 13:31:42 -0500

    first commit

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name Function"
+}
+
+variable "location" {
+  type        = string
+  default     = "West Europe"
+  description = "Location"
+}
+
+variable "user" {
+  type        = string
+  default     = "admin1234"
+  description = "Name Function"
+}
+
+variable "password" {
+  type        = string
+  default     = "Halo1184#"
+  description = "Name Function"
+}
